8f233c6b8431f3fda9b1b1f3c8f43f59f7fcb544
melecito <ahimelecc123@gmail.com>
2025-10-24 20:39:37 -0500

    Ajuste del environment

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -4,5 +4,5 @@
 
 export const environment = {
   production: true, // Debe ser 'true' para producción
-  apiUrl: "https://todo-app-backend-wtq8.onrender.com"
+  apiUrl: "https://todo-backend-production-d3b2.up.railway.app/"
 }
